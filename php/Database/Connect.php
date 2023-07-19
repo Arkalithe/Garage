a9aff5ddc05b8c6c9c92f6f0d41efc8444021e29
Vigneron Tristan <arkalithe@gmail.com>
2023-07-19 22:10:21 +0200

    database connection

--- php/Database/Connect.php
+++ php/Database/Connect.php
@@ -2,23 +2,22 @@
 
 class DatabaseConnect
 {
-    private $serverName = "i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-    private $userName = "ql46xy00vxcox0rc";
-    private $password = "xzeto0sv9h22h0er";
-    private $dbname = "clryze4bwb99689n";
-    private $port ="3306";
+    private $dsn = 'mysql:host=i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=clryze4bwb99689n';
+    private $username = 'ql46xy00vxcox0rc';
+    private $password = 'xzeto0sv9h22h0er';
+
     public $conn;
 
     public function __construct()
     {
-        
+        $this->dbConnection();
     }
     
     public function dbConnection()
     {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->serverName . ";port=" . $this->port, $this->userName, $this->password);
+            $this->conn = new PDO($this->dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);            
             
         } catch (PDOException $e) {
@@ -31,7 +30,7 @@ class DatabaseConnect
     {   
         $this->conn = null;
         try {
-            $this->conn =  new PDO("mysql:host=".$this->serverName.";dbname=".$this->dbname,$this->userName, $this->password);
+            $this->conn =  new PDO($this->dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);            
         } catch (PDOException $e) {
             echo "Connection Raté : " . $e->getMessage();            
