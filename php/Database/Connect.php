db859422233f003616b36a56b6e5596e01951e7e
Vigneron Tristan <arkalithe@gmail.com>
2023-07-19 21:52:10 +0200

    database change

--- php/Database/Connect.php
+++ php/Database/Connect.php
@@ -2,10 +2,11 @@
 
 class DatabaseConnect
 {
-    private $serverName = "localhost";
-    private $userName = "root";
-    private $password = "";
-    private $dbname = "garagevparrot";
+    private $serverName = "i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+    private $userName = "ql46xy00vxcox0rc";
+    private $password = "xzeto0sv9h22h0er ";
+    private $dbname = "clryze4bwb99689n";
+    private $port ="3306";
     public $conn;
 
     public function __construct()
@@ -17,7 +18,7 @@ class DatabaseConnect
     {
         $this->conn = null;
         try {
-            $this->conn =  new PDO("mysql:host=".$this->serverName.";",$this->userName, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->serverName . ";port=" . $this->port, $this->userName, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);            
             
         } catch (PDOException $e) {
