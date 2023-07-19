fcdda2bd7b18e7c04a9f0210f6e22d7b98dd1dd5
Vigneron Tristan <arkalithe@gmail.com>
2023-07-20 00:54:09 +0200

    Databse connect

--- php/Database/Connect.php
+++ php/Database/Connect.php
@@ -2,9 +2,9 @@
 
 class DatabaseConnect
 {
-    private $dsn = 'mysql:host=i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=clryze4bwb99689n';
-    private $username = 'ql46xy00vxcox0rc';
-    private $password = 'vqw7dh2fbvuzcbt0';
+    private $dsn;
+    private $username;
+    private $password;
 
     public $conn;
 
@@ -16,18 +16,25 @@ class DatabaseConnect
     public function dbConnection()
     {
         $this->conn = null;
-        try {      
+        try {
             $databaseUrl = getenv('JAWSDB_URL');
-            $urlParts = parse_url($databaseUrl);
-            $this->dsn = 'mysql:host=' . $urlParts['host'] . ';port=' . $urlParts['port'] . ';dbname=' . ltrim($urlParts['path'], '/');
-            $this->username = $urlParts['user'];
-            $this->password = $urlParts['pass'];
+            if ($databaseUrl) {
+                $urlParts = parse_url($databaseUrl);
+                $this->dsn = 'mysql:host=' . $urlParts['host'] . ';port=' . $urlParts['port'] . ';dbname=' . ltrim($urlParts['path'], '/');
+                $this->username = $urlParts['user'];
+                $this->password = $urlParts['pass'];
+            } else {
+                $this->dsn = 'mysql:host=i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=clryze4bwb99689n';
+                $this->username = 'ql46xy00vxcox0rc';
+                $this->password = 'vqw7dh2fbvuzcbt0';
+            }
+
             $this->conn = new PDO($this->dsn, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);       
-           
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            echo "Connection Raté : " . $e->getMessage();            
+            echo "Connection Failed: " . $e->getMessage();
         }
+
         return $this->conn;
     }
 
@@ -35,13 +42,24 @@ class DatabaseConnect
     {   
         $this->conn = null;
         try {
-            $this->conn =  new PDO($this->dsn, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-          
+            $databaseUrl = getenv('JAWSDB_URL');
+            if ($databaseUrl) {
+                $urlParts = parse_url($databaseUrl);
+                $this->dsn = 'mysql:host=' . $urlParts['host'] . ';port=' . $urlParts['port'] . ';dbname=' . ltrim($urlParts['path'], '/');
+                $this->username = $urlParts['user'];
+                $this->password = $urlParts['pass'];
+            } else {
+                $this->dsn = 'mysql:host=i54jns50s3z6gbjt.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;port=3306;dbname=clryze4bwb99689n';
+                $this->username = 'ql46xy00vxcox0rc';
+                $this->password = 'vqw7dh2fbvuzcbt0';
+            }
+
+            $this->conn = new PDO($this->dsn, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            echo "Connection Raté : " . $e->getMessage();            
+            echo "Connection Failed: " . $e->getMessage();
         }
+
         return $this->conn;
     }
-
 }
