321144b0c429dda120a8b48b73d3147cbdc4cff1
Vigneron Tristan <arkalithe@gmail.com>
2023-07-20 02:04:46 +0200

    AdminCree

--- php/AdminCree.php
+++ php/AdminCree.php
@@ -1,4 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET,POST,");
+header('Access-Control-Allow-Credentials: true');
+header('Content-Type: plain/text');
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Methods,Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization, X-Requested-With");
+
+include_once '../../Database/Connect.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $motDePasse = $_POST["mot_de_passe"];
     $email = $_POST["email"];
