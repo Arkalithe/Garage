6a262010b83e6e71bd2293ed5d3b52ddbd355fda
Vigneron Tristan <arkalithe@gmail.com>
2023-07-20 10:57:15 +0200

    header AdminCree

--- php/AdminCree.php
+++ php/AdminCree.php
@@ -2,7 +2,6 @@
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: GET,POST,");
 header('Access-Control-Allow-Credentials: true');
-header('Content-Type: plain/text');
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Methods,Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization, X-Requested-With");
 
 include_once './Database/Connect.php';
