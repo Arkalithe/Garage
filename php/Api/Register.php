786cea73391bea8d8058d6261d900cf0c49c517b
Vigneron Tristan <arkalithe@gmail.com>
2023-07-20 14:44:52 +0200

    header origin

--- php/Api/Register.php
+++ php/Api/Register.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://imaginative-lollipop-cdaa75.netlify.app");
 header("Access-Control-Allow-Methods: GET,POST");
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Headers: access");
--- php/Api/Reparation/ReparationUpdate.php
+++ php/Api/Reparation/ReparationUpdate.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://imaginative-lollipop-cdaa75.netlify.app");
 header("Access-Control-Allow-Methods: GET,POST,PUT");
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Authorization, X-Requested-With");
