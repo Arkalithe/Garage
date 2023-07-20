5dd4c46749d21014743d0427db6c2348bf9628f8
Vigneron Tristan <arkalithe@gmail.com>
2023-07-20 15:38:29 +0200

    making sure only one component appear each time

--- src/components/Reparation/Reparation.js
+++ src/components/Reparation/Reparation.js
@@ -23,52 +23,49 @@ const Reparation = () => {
     }
   };
 
-  const contents = reparationContent.map((Content) => (
-
-
-    <div className="card form-cadre h-100" key={Content.id}>
-      <div className="pb-2 card-title">
-        <h1>{Content.title}</h1>
-      </div>
-      <div className="pb-3 m-2">
-        {Content.image.length > 0 ? (
-          <div
-            className="image-container"
-            style={{
-              maxWidth: "300px",
-              maxHeight: "300px",
-              overflow: "hidden",
-            }}
-          >
-            <img
-              className="img-fluid"
-              src={require(`../../assests/Image/${Content.image}`)}
-              alt="Reparation"
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-              }}
-            />
-          </div>
-        ) : (
-          <div>No Image</div>
-        )}
-      </div>
-      <div className="pb-3 ">
-        <p>{Content.intro}</p>
-      </div>
-
-      <Link to={"/reparation"} className="d-flex justify-content-center mt-auto bouton bouton-lien">
-        Plus d'information
-      </Link>
-
-    </div>
-  ))
-
+  const firstReparation = reparationContent.length > 0 ? reparationContent[0] : null;
 
   return (
-    <div className="col mb-4">{contents}</div>
+    <div className="col mb-4">
+      {firstReparation && (
+        <div className="card form-cadre h-100">
+          <div className="pb-2 card-title">
+            <h1>{firstReparation.title}</h1>
+          </div>
+          <div className="pb-3 m-2">
+            {firstReparation.image.length > 0 ? (
+              <div
+                className="image-container"
+                style={{
+                  maxWidth: "300px",
+                  maxHeight: "300px",
+                  overflow: "hidden",
+                }}
+              >
+                <img
+                  className="img-fluid"
+                  src={require(`../../assests/Image/${firstReparation.image}`)}
+                  alt="Reparation"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+            ) : (
+              <div>No Image</div>
+            )}
+          </div>
+          <div className="pb-3">
+            <p>{firstReparation.intro}</p>
+          </div>
+          <Link to={"/reparation"} className="d-flex justify-content-center mt-auto bouton bouton-lien">
+            Plus d'information
+          </Link>
+        </div>
+      )}
+    </div>
   );
 };
 
