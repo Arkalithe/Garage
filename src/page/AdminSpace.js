6901512f4943dc8d1ca1c7e5be96adf71c4258dc
Arkalithe <arkalithe@gmail.com>
2024-05-25 14:46:04 +0200

    Bootstrap admin panel update

--- src/page/AdminSpace.js
+++ src/page/AdminSpace.js
@@ -1,48 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const AdminSpace = () => {
   return (
-    <Container className='form-cadre p-1'>
-      <div className='d-flex flex-column'>
+    <Container className='form-cadre p-1'>      
         <Row className='justify-content-center'>
-
-          <Col lg={5} md={5} sm={12} className='d-flex flex-column align-items-center cadre-admin m-1'>
+          <Col lg={5} md={5} sm={12} className=' cadre-admin m-2'>
             <h3>Gestion Employé</h3>
-            <Link to='/employe'><Button variant='primary'>Modifié</Button></Link>
+            <Link to='/employe'><button  className='mb-3 bouton bouton-lien'>Modifier</button></Link>
           </Col>
 
-          <Col lg={5} md={5} sm={12} className='d-flex flex-column align-items-center cadre-admin m-1'>
+          <Col lg={5} md={5} sm={12} className='cadre-admin m-2'>
             <h3>Gestion Horaire</h3>
-            <Link to='/horaire'><Button variant='primary'>Modifié</Button></Link>
+            <Link to='/horaire'><button  className='mb-3 bouton bouton-lien'>Modifier</button></Link>
           </Col>
         </Row>
 
         <Row className='justify-content-center'>
-          <Col lg={5} md={5} sm={12} className='d-flex flex-column align-items-center cadre-admin m-1'>
+          <Col lg={5} md={5} sm={12} className='cadre-admin m-2'>
             <h3>Moderation avis</h3>
-            <Link to='/avis'><Button variant='primary'>Modifié</Button></Link>
+            <Link to='/avis'><button  className='mb-3 bouton bouton-lien'>Modifier</button></Link>
           </Col>
 
-          <Col lg={5} md={5} sm={12} className='d-flex flex-column align-items-center cadre-admin m-1'>
+          <Col lg={5} md={5} sm={12} className='cadre-admin m-2'>
             <h3>Creation Voiture</h3>
-            <Link to='/creationVoiture'><Button variant='primary'>Modifié</Button></Link>
+            <Link to='/creationVoiture'><button className='mb-3 bouton bouton-lien'>Modifier</button></Link>
           </Col>
         </Row>
 
         <Row className='justify-content-center'>
-          <Col lg={5} md={5} sm={12} className='d-flex flex-column align-items-center cadre-admin m-1'>
+          <Col lg={5} md={5} sm={12} className='cadre-admin m-2'>
             <h3>Moderation Voiture</h3>
-            <Link to='/updateVoiture'><Button variant='primary'>Modifié</Button></Link>
+            <Link to='/updateVoiture'><button  className='mb-3 bouton bouton-lien'>Modifier</button></Link>
           </Col>
 
-          <Col lg={5} md={5} sm={12} className='d-flex flex-column align-items-center cadre-admin m-1'>
+          <Col lg={5} md={5} sm={12} className='cadre-admin m-2'>
             <h3>Moderation Contenue</h3>
-            <Link to='/contentSpace'><Button variant='primary'>Modifié</Button></Link>
+            <Link to='/contentSpace'><button className='mb-3 bouton bouton-lien'>Modifier</button></Link>
           </Col>
         </Row>
-      </div>
+      
     </Container>
   );
 };
