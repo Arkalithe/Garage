50fdb7df2f1aefa72693d8fafe4ad7183babf45e
Arkalithe <arkalithe@gmail.com>
2024-06-02 13:43:01 +0200

    changement location jwt_secret sous forme de variable d'environement.

--- php/Class/JwtHandler.php
+++ php/Class/JwtHandler.php
@@ -4,10 +4,11 @@ require '../vendor/autoload.php';
 use Firebase\JWT\JWT;
 use Firebase\JWT\ExpiredException;
 use Firebase\JWT\Key;
+use Dotenv\Dotenv;
 
 class JwtHandler
 {
-    protected $jwt_secrect;
+    protected $jwt_secret;
     protected $token;
     protected $issuedAt;
     protected $expire;
@@ -15,50 +16,49 @@ class JwtHandler
 
     public function __construct()
     {
-        
-        date_default_timezone_set('Europe/Paris');
-        $this->issuedAt = time();        
-        $this->expire = $this->issuedAt + 3600;       
-        $this->jwt_secrect = "clef_secret";
+        // Charger les variables d'environnement
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+
+        date_default_timezone_set('Europe/Paris');  // Définit le fuseau horaire par défaut
+        $this->issuedAt = time();  // Timestamp actuel
+        $this->expire = $this->issuedAt + 3600;  // Expiration dans 1 heure
+        $this->jwt_secret = $_ENV['JWT_SECRET'];  // Utilisation de la variable d'environnement pour la clé secrète
     }
 
+    // Méthode pour encoder les données en JWT
     public function jwtEncodeData($iss, $data)
     {
-
         $this->token = array(
-            
-            "iss" => $iss,
-            "aud" => $iss,            
-            "iat" => $this->issuedAt,            
-            "exp" => $this->expire,            
-            "data" => $data
+            "iss" => $iss,  // Émetteur
+            "aud" => $iss,  // Audience
+            "iat" => $this->issuedAt,  // Temps d'émission
+            "exp" => $this->expire,  // Temps d'expiration
+            "data" => $data  // Données de l'utilisateur
         );
 
-        $this->jwt = JWT::encode($this->token, $this->jwt_secrect, 'HS256');
+        $this->jwt = JWT::encode($this->token, $this->jwt_secret, 'HS256');
         return $this->jwt;
     }
 
+    // Méthode pour décoder le JWT
     public function jwtDecodeData($jwt_token)
     {
         try {
-            $decode = JWT::decode($jwt_token, new Key($this->jwt_secrect , 'HS256')); 
+            // Décodage du token
+            $decode = JWT::decode($jwt_token, new Key($this->jwt_secret, 'HS256'));
             $currentTimestamp = time();
+
+            // Si le token est expiré,
             if ($currentTimestamp > $decode->exp) {
                 echo "<script>localStorage.removeItem('accessToken');</script>";
-                throw new ExpiredException('Token has expired');
+                throw new ExpiredException('Le token a expiré');
             }
-            
-            return [
-                "data" => $decode->data
-            ];
+            return ["data" => $decode->data];
         } catch (ExpiredException $e) {
-            return [
-                "message" => $e->getMessage()
-            ];
+            return ["message" => $e->getMessage()];
         } catch (Exception $e) {
-            return [
-                "message" => $e->getMessage()
-            ];
+            return ["message" => $e->getMessage()];
         }
     }
 }
